cf0efd747e62008621a8f1182ac990add44c172a
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-10-09 20:19:39 -0700

    added json code

--- index.js
+++ index.js
@@ -13,36 +13,57 @@ function questionPrompt() {
         },
         {
             type: "input",
-            message: "What is the name of your project?",
+            message: "What is the title of your project?",
             name: "title"
         },
         {
             type: "input",
-            message: "What is your LinkedIn URL?",
-            name: "linkedin"
+            message: "Please enter a brief description of your project.",
+            name: "description"
         },
         {
             type: "input",
-            message: "Do you have a GitHub URL?",
-            name: "github"
+            message: "Please enter what needs to be installed and brief instructions.",
+            name: "installationInstructions"
+        },
+        {
+            type: "input",
+            message: "What kind of tests did you perform?",
+            name: "tests"
+        },
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "githubname"
+        },
+        {
+            type: "input",
+            message: "What is your GitHub email?",
+            name: "githubemail"
+        },
+        {
+            type: "input",
+            message: "What kind of license are you going to use?",
+            name: "license"
         }
-    ])}
+    ])
+}
 
-    questionPrompt().then(function(userReply){
-        console.log(userReply)
-        const readme = generateMD(userReply);
+questionPrompt().then(function (userReply) {
+    console.log(userReply)
+    const readme = generateMD(userReply);
 
-        return writeFileAsync("./readmeoutput/README.md", readme)
-    })
-    .then(function() {
+    return writeFileAsync("./readmeoutput/README.md", readme)
+})
+    .then(function () {
         console.log("Success writing README")
     })
-    .catch(function(err) {
+    .catch(function (err) {
         console.log(err);
     })
 
-    function generateMD(userReply) {
-        return `
+function generateMD(userReply) {
+    return `
         # ${userReply.title}
 
         ## Description
@@ -52,24 +73,30 @@ function questionPrompt() {
         - [Installation](#installation)
         - [Usage](#usage)
         - [License](#license)
+        - [Contributing](#contributing)
 
         ##Installation
+
+        ${userReply.installationInstructions}
 
         ## Usage
 
         ## License
+        * ${userReply.license} license
 
         ## Contributing
         * **${userReply.name}** 
-
+        
         ## Tests   
+        ``````
+        ${userReply.tests}
 
         ## Questions
-        [GithubImage](https://github.com/${userReply.githubname}.png)
-            ${userReply.githubemail}
+        ![GithubImage](https://github.com/${userReply.githubname}.png)
+        [GithubEmail](mailto:${userReply.githubemail})    
 
         ##Badge
         ![badge](https://img.shields.io/github/license/${userReply.githubname}/${userReply.title})
 
         `
-    }
+}
